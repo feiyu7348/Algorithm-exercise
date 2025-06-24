a2e2e16f0f875118dae04cb097a929004b61f407
zfy <aeiuuu@163.com>
2025-06-25 00:02:49 +0800

    斐波那契数列

--- README.md
+++ README.md
@@ -23,3 +23,5 @@
 ## 贪心算法
 
 ## 动态规划
+
+1.  [斐波那契数列](https://programmercarl.com/0509.%E6%96%90%E6%B3%A2%E9%82%A3%E5%A5%91%E6%95%B0.html#%E7%AE%97%E6%B3%95%E5%85%AC%E5%BC%80%E8%AF%BE)
---
+++ golang/dp/斐波那契数列.go
@@ -0,0 +1,26 @@
+package dp
+
+func Fibonacci(n int) int {
+	if n <= 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	}
+	return Fibonacci(n-1) + Fibonacci(n-2)
+}
+
+func FibonacciDP(n int) int {
+	if n <= 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	}
+
+	dp := make([]int, n+1)
+	dp[0] = 0
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
