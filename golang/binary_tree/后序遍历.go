3debf5b0a5c33840da1c390869d9ac325c043290
zfy <aeiuuu@163.com>
2025-06-27 23:17:33 +0800

    后序遍历

--- golang/binary_tree/后序遍历.go
+++ golang/binary_tree/后序遍历.go
@@ -1,5 +1,17 @@
 package binarytree
 
-// func postorderTraversal(root *TreeNode) []int {
+func postorderTraversal(root *TreeNode) []int {
+	var res []int
+	var traversal func(*TreeNode)
+	traversal = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		traversal(node.Left)
+		traversal(node.Right)
+		res = append(res, node.Val)
+	}
 
-// }
+	traversal(root)
+	return res
+}
