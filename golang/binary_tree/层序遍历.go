c7e297d7a314caf78c5b5cd23833a74dcddafea5
zfy <aeiuuu@163.com>
2025-06-28 18:12:44 +0800

    二叉树层序遍历

--- golang/binary_tree/层序遍历.go
+++ golang/binary_tree/层序遍历.go
@@ -1,5 +1,24 @@
 package binarytree
 
-// func levelOrder(root *TreeNode) [][]int {
+func levelOrder(root *TreeNode) [][]int {
+	res := [][]int{}
+	depth := 0
 
-// }
+	var order func(node *TreeNode, depth int)
+	order = func(node *TreeNode, depth int) {
+		if node == nil {
+			return
+		}
+
+		if len(res) == depth {
+			res = append(res, []int{})
+		}
+
+		res[depth] = append(res[depth], node.Val)
+		order(node.Left, depth+1)
+		order(node.Right, depth+1)
+	}
+
+	order(root, depth)
+	return res
+}
