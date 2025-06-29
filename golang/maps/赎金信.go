2d5796ddf555ca5dc7b90f9c080f02ba60d4ced8
zfy <aeiuuu@163.com>
2025-06-29 21:20:24 +0800

    赎金信

--- golang/maps/赎金信.go
+++ golang/maps/赎金信.go
@@ -1,5 +1,16 @@
 package maps
 
-// func canConstruct(ransomNote string, magazine string) bool {
+func canConstruct(ransomNote string, magazine string) bool {
+	record := [26]rune{}
+	for _, v := range magazine {
+		record[v-'a']++
+	}
 
-// }
+	for _, v := range ransomNote {
+		record[v-'a']--
+		if record[v-'a'] < 0 {
+			return false
+		}
+	}
+	return true
+}
