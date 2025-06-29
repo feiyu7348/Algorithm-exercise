84915dc28b91bbee0e6fe07d590b98222d69662c
zfy <aeiuuu@163.com>
2025-06-29 17:29:42 +0800

    两数之和

--- golang/maps/两数之和.go
+++ golang/maps/两数之和.go
@@ -1,5 +1,13 @@
 package maps
 
-// func twoSum(nums []int, target int) []int {
-
-// }
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, v := range nums {
+		if j, ok := m[target-v]; ok {
+			return []int{i, j}
+		} else {
+			m[v] = i
+		}
+	}
+	return nil
+}
