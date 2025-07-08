7d1e7211381348536b60389944e5fe2090584214
zfy <aeiuuu@163.com>
2025-07-08 17:13:02 +0800

    压缩字符串

---
+++ golang/strings/字符串压缩.go
@@ -0,0 +1,33 @@
+package strings
+
+import "strconv"
+
+func compressString(S string) string {
+	n := len(S)
+	if n == 0 {
+		return S
+	}
+
+	var newS []byte
+	curChar := S[0]
+	count := 1
+	for i := 1; i < n; i++ {
+		if S[i] == curChar {
+			count++
+		} else {
+			newS = append(newS, curChar)
+			newS = append(newS, strconv.Itoa(count)...)
+			curChar = S[i]
+			count = 1
+		}
+	}
+
+	newS = append(newS, curChar)
+	newS = append(newS, strconv.Itoa(count)...)
+	newSS := string(newS)
+
+	if len(newSS) >= n {
+		return S
+	}
+	return newSS
+}
