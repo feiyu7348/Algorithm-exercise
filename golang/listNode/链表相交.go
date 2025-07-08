53035d2f69e49ce34ed9ce93bbeaac8dda81437f
zfy <aeiuuu@163.com>
2025-07-08 11:22:02 +0800

    链表相交

--- golang/listNode/链表相交.go
+++ golang/listNode/链表相交.go
@@ -1,5 +1,36 @@
 package listNode
 
-// func getIntersectionNode(headA, headB *ListNode) *ListNode {
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	m, n := 0, 0
+	nodeA, nodeB := headA, headB
+	for nodeA != nil {
+		nodeA = nodeA.Next
+		m++
+	}
 
-// }
+	for nodeB != nodeA {
+		nodeB = nodeB.Next
+		n++
+	}
+
+	var step int
+	var slow, fast *ListNode
+	if m > n {
+		step = m - n
+		fast, slow = headA, headB
+	} else {
+		step = n - m
+		fast, slow = headB, headA
+	}
+
+	for i := 0; i < step; i++ {
+		fast = fast.Next
+	}
+
+	for fast != slow {
+		slow = slow.Next
+		fast = fast.Next
+	}
+
+	return fast
+}
