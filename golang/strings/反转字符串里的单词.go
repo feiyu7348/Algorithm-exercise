adc3b8d5dc21de044efdd54947424701300cc6a9
zfy <aeiuuu@163.com>
2025-07-08 16:49:42 +0800

    反转字符串里的单词

---
+++ golang/strings/反转字符串里的单词.go
@@ -0,0 +1,32 @@
+package strings
+
+func reverseWords(s string) string {
+	n := len(s)
+	b := []byte(s)
+	slow := 0
+	for i := 0; i < n; i++ {
+		if b[i] != ' ' {
+			if slow != 0 {
+				b[slow] = ' '
+				slow++
+			}
+			for i < n && b[i] != ' ' {
+				b[slow] = b[i]
+				slow++
+				i++
+			}
+		}
+	}
+	b = b[:slow]
+
+	reverseString(b)
+	last := 0
+	for i := 0; i <= len(b); i++ {
+		if i == len(b) || b[i] == ' ' {
+			reverseString(b[last:i])
+			last = i + 1
+		}
+	}
+
+	return string(b)
+}
