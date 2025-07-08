86b7c6183b2bc417cdff1cd66db452add4ee2dfa
zfy <aeiuuu@163.com>
2025-07-08 16:26:43 +0800

    反转字符串

---
+++ golang/strings/反转字符串Ⅱ.go
@@ -0,0 +1,15 @@
+package strings
+
+func reverseStr(s string, k int) string {
+	n := len(s)
+	b := []byte(s)
+	for i := 0; i < n; i = i + 2*k {
+		if i+k <= n {
+			reverseString(b[i : i+k])
+		} else {
+			reverseString(b[i:n])
+		}
+	}
+
+	return string(b)
+}
